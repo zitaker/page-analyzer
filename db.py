d9199d46ecac1daa6243c6d5d3984566bd01b2a4
georg <georbearwolf.com>
2023-08-31 10:08:37 +0300

    [019] Implement an address entry form on the main page and a handler that adds the entered information to the database

--- db.py
+++ db.py
@@ -1,15 +1,56 @@
+# import psycopg2
+# import os
+#
+#
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# print(DATABASE_URL)
+# print('11111111111111111111')
+# conn = psycopg2.connect(DATABASE_URL)
+# print('22222222222222222222222222')
+# print(conn)
+# print('22222222222222222222222222')
+#
+# try:
+#     # пытаемся подключиться к базе данных
+#     conn = psycopg2.connect(dbname='database', user='postgres', password='postgres')
+#     print('подключил SQL')
+#     # conn = psycopg2.connect(DATABASE_URL)
+#     # conn = psycopg2.connect('postgresql://postgres:postgres@host:port/database')
+# except:
+#     # в случае сбоя подключения будет выведено сообщение в STDOUT
+#     print('Can`t establish connection to database')
+
 import psycopg2
-import os
+from psycopg2 import OperationalError
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+        print('работает')
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        print('не работает')
+    return connection
 
-try:
-    # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(dbname='database', user='postgres', password='postgres')
-    # conn = psycopg2.connect(DATABASE_URL)
-    # conn = psycopg2.connect('postgresql://postgres:postgres@host:port/database')
-except:
-    # в случае сбоя подключения будет выведено сообщение в STDOUT
-    print('Can`t establish connection to database')
+conn = create_connection(
+    "database", "postgres", "postgres", "127.0.0.1", "5432"
+)
+
+
+# # получение объекта курсора
+# cursor = conn.cursor()
+#
+# # Получаем список всех пользователей
+# cursor.execute('SELECT * FROM users')
+# all_users = cursor.fetchall()
+# cursor.close() # закрываем курсор
+# conn.close() # закрываем соединение
 
