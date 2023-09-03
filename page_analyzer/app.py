0281e6c26672a691229639e9128300882d6e38ee
georg_dryndin <georbearwolf.com>
2023-09-03 14:55:53 +0300

    [024] Implement the address entry form on the main page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask import render_template
 
+import psycopg2
+import os
 
 app = Flask(__name__)
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route('/')
 def index():
-    # return render_template('index.html', input_data=insert_into_urls())
-    return render_template('index.html')
+    try:
+        conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
+                                host='127.0.0.1', port='5432')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO urls (name) VALUES ('qwerty9')")
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return render_template('index.html')
+
+    except:
+        print('ошибка SQL. Can`t establish connection to database')
 
 
 if __name__ == '__main__':
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -5,25 +5,26 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 
-try:
-    # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
-                            host='127.0.0.1', port='5432')
-    print('подключил SQL')
-
-except:
-    # в случае сбоя подключения будет выведено сообщение в STDOUT
-    print('Can`t establish connection to database')
-
 def insert_into_urls():
-    cursor = conn.cursor()
-    # добавляем строку в таблицу people
-    cursor.execute("INSERT INTO urls (name) VALUES ('qwerty5')")
-    # выполняем транзакцию
-    conn.commit()
-    print("Данные добавлены")
-    cursor.close()
-    conn.close()
+    try:
+        conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
+                                host='127.0.0.1', port='5432')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO urls (name) VALUES ('qwerty5')")
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    except:
+        print('ошибка SQL. Can`t establish connection to database')
+
+# def insert_into_urls():
+#     cursor = conn.cursor()
+#     cursor.execute("INSERT INTO urls (name) VALUES ('qwerty5')")
+#     conn.commit()
+#     print("Данные добавлены")
+#     cursor.close()
+#     conn.close()
 
 
 # print(insert_into_urls())
