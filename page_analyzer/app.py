6cee11e9e7e4f6dc9faf4526bcfafa0ff14cac14
georg <georbearwolf.com>
2023-09-05 16:34:38 +0300

    [029] sending to the database from the form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from .db import insert_into_urls
+from flask import request
 
 
 app = Flask(__name__)
@@ -9,6 +10,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html'), insert_into_urls()
+
+
+@app.route('/urls', methods=['POST', 'GET'])
+def page_urls():
+    if request.method == 'POST':
+        result = request.form.get('url')
+    return render_template('urls.html', result=result)
 
 
 if __name__ == '__main__':
