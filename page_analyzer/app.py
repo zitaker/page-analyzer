3a624202e0c2e06667aaaa252dce01ccc11dacc7
georg <georbearwolf.com>
2023-09-06 12:22:34 +0300

    [030] sending to the database from the form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,21 +3,36 @@ from flask import render_template
 from .db import insert_into_urls
 from flask import request
 
+import psycopg2
+import os
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html'), insert_into_urls()
+    # return render_template('index.html'), insert_into_urls()
+    return render_template('index.html')
 
 
 @app.route('/urls', methods=['POST', 'GET'])
 def page_urls():
     if request.method == 'POST':
-        result = request.form.get('url')
-    return render_template('urls.html', result=result)
 
+        conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
+                                host='127.0.0.1', port='5432')
+        cursor = conn.cursor()
+        # cursor.execute("INSERT INTO urls (name) VALUES ('qwerty33')")
+        cursor.execute("INSERT INTO urls (name) VALUES ({{}})")
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        result = request.form.get('url')
+        return render_template('urls.html', result=result)
+    return render_template('urls.html')
 
 if __name__ == '__main__':
     app.run()
