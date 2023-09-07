ca501d659350d39216b3612d995c9343847802ee
georg <georbearwolf.com>
2023-09-07 17:09:17 +0300

    [034] fitting data to a table

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from flask import render_template
-from .db import insert_into_urls
-from flask import request
-
 import psycopg2
 import os
+
+from flask import Flask
+from flask import render_template
+# from .db import sql_connection
+from flask import request
+
+
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 
@@ -13,11 +15,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return render_template('index.html'), insert_into_urls()
     return render_template('index.html')
 
 
-@app.route('/urls', methods=['POST', 'GET'])
+@app.route('/urls/', methods=['POST', 'GET'])
 def page_urls():
     if request.method == 'POST':
         try:
@@ -30,6 +31,25 @@ def page_urls():
             cursor.close()
             conn.close()
             return render_template('show.html', result=result)
+        except:
+            print('ошибка SQL. Can`t establish connection to database')
+
+
+@app.route('/urls', methods=['POST', 'GET'])
+def urls():
+    if request.method == 'GET':
+        try:
+            conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
+                                    host='127.0.0.1', port='5432')
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM urls;")
+            all_users = cursor.fetchall()
+
+            cursor.close()
+            conn.close()
+            return render_template('urls.html', result1=all_users)
+
+
         except:
             print('ошибка SQL. Can`t establish connection to database')
     return render_template('urls.html')
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -10,13 +10,18 @@ def insert_into_urls():
         conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
                                 host='127.0.0.1', port='5432')
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO urls (name) VALUES ('qwerty33')")
-        conn.commit()
+        # cursor.execute("INSERT INTO urls (name) VALUES ('qwerty33')")
+        cursor.execute("SELECT * FROM urls;")
+        all_users = cursor.fetchall()
+        # conn.commit()
         cursor.close()
         conn.close()
+        return all_users
 
     except:
         print('ошибка SQL. Can`t establish connection to database')
+
+print(insert_into_urls())
 
 
 
