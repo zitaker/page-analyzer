549dc2da5dabb6405410a4ae4e9499ca1ec9518e
georg <georbearwolf.com>
2023-09-11 13:39:26 +0300

    [036] add by id in urls page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -43,36 +43,12 @@ def urls():
             conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
                                     host='127.0.0.1', port='5432')
 
-            # with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            #     curs.execute("SELECT (id) FROM urls;")
-            #     id_urls = curs.fetchall()
-            # for i in range(len(id_urls)):
-            #     id_atr = id_urls[i]
-
             with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
                 curs.execute('SELECT * FROM urls ORDER BY id DESC;')
                 rows = curs.fetchall()
-
-                id_atr = []
-                for row in rows:
-                    id_atr.append((row[0]))
-
-                name_atr = []
-                for row in rows:
-                    name_atr.append((row[1]))
-
-                created_at_atr = []
-                for row in rows:
-                    created_at_atr.append((row[2]))
-
-            conn.close()
-            return render_template(
-                'urls.html',
-                id_atr=id_atr[0],
-                name_atr=name_atr,
-                created_at_atr=created_at_atr)
-            # return render_template('urls.html', context={'all_users': all_users})
-
+                conn.close()
+                return render_template(
+                'urls.html', rows=rows)
 
         except:
             print('ошибка SQL. Can`t establish connection to database')
