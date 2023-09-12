3b4cf64772da66acc52702cd22c0a9ea19eff6d0
georg <georbearwolf.com>
2023-09-12 15:45:13 +0300

    [038] get a row from the table by id

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -7,6 +7,8 @@ from flask import render_template
 from flask import request
 from psycopg2.extras import NamedTupleCursor
 
+from flask import url_for
+import json
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
@@ -19,26 +21,44 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/urls/', methods=['POST', 'GET'])
+@app.route('/urls/', methods=['POST'])
 def page_urls():
     if request.method == 'POST':
         try:
             conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
                                     host='127.0.0.1', port='5432')
-            result = request.form.get('url')
+            get_request_form = request.form.get('url')
             with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-                curs.execute("INSERT INTO urls (name) VALUES (%s)", [result])
+                curs.execute("INSERT INTO urls (name) VALUES (%s)", [get_request_form])
                 conn.commit()
 
-                curs.execute('SELECT * FROM urls ORDER BY id DESC;')
-                row = curs.fetchmany(size=1)
+                # curs.execute('SELECT * FROM urls ORDER BY id DESC;')
+                # row = curs.fetchmany(size=1)
                 conn.close()
-            return render_template('show.html', result=result, row=row)
+            # return render_template('show.html', row=row)
+            # return render_template('show.html')
+            return render_template('/urls/<int:username>')
         except:
             print('ошибка SQL. Can`t establish connection to database')
 
+@app.route('/urls/<int:username>')
+def get_urls(username):
+    try:
+        conn = psycopg2.connect(dbname='database', user='postgres', password='postgres',
+                                host='127.0.0.1', port='5432')
+        with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+            curs.execute('SELECT * FROM urls ORDER BY id DESC;')
+            row = curs.fetchmany(size=1)
+            conn.close()
+    except:
+        print('ошибка SQL. Can`t establish connection to database')
+    # return json.dumps(username)
 
-@app.route('/urls', methods=['POST', 'GET'])
+    return render_template('show.html', row=row), json.dumps(username)
+
+
+
+@app.route('/urls', methods=['GET'])
 def urls():
     if request.method == 'GET':
         try:
