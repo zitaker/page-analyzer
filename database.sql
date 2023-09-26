583e82f44fde376ac5dc8c1e485ef4def8f8b9b1
georg <georbearwolf.com>
2023-09-26 14:00:06 +0300

    [070]

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
---DROP TABLE IF EXISTS url_checks;
---DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
+DROP TABLE IF EXISTS urls;
 
---CREATE TABLE urls (
---    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---    name varchar(255) unique NOT NULL,
---    created_at DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
---);
+CREATE TABLE urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) unique NOT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 
 --INSERT INTO urls (name) VALUES ('Arya');
