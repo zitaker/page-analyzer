010683ef7d784c41a7cd1d51f7fc370825e4495d
georg_dryndin <georbearwolf.com>
2023-10-01 21:26:30 +0300

    [080] output id as a jinja2 foreign key - specify this in python

--- database.sql
+++ database.sql
@@ -10,7 +10,14 @@ CREATE TABLE urls (
 
 CREATE TABLE url_checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--    url_checks text,
+
     url_id bigint REFERENCES urls (id),
+--    url_id bigint REFERENCES urls(id),
+--    FOREIGN KEY url_id REFERENCES urls (id),
+--    ALTER TABLE url_checks ADD FOREIGN KEY (url_id) REFERENCES urls(id),
+--    FOREIGN KEY (url_id) REFERENCES urls(id),
+--    ALTER TABLE (url_id) bigint REFERENCES urls(id),
     status_code numeric,
     h1 text,
     title text,
@@ -19,12 +26,13 @@ CREATE TABLE url_checks (
 );
 
 
---INSERT INTO url_checks (h1) VALUES ('qwerty');
+
 
 --INSERT INTO urls (name) VALUES ('Arya');
 --INSERT INTO urls (name) VALUES ('Bron');
 
+--INSERT INTO url_checks (h1) VALUES ('qwerty');
+--INSERT INTO url_checks (url_id) VALUES (2);
 
-
---SELECT * FROM urls;
+SELECT * FROM urls;
 SELECT * FROM url_checks;
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -75,9 +75,12 @@ def get_urls(id):
         flash('Страница успешно проверена', category='success')
 
         with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            curs.execute("INSERT INTO url_checks (h1) VALUES ('111')")
+            # curs.execute("INSERT INTO url_checks (h1) VALUES ('111')")
+            curs.execute("INSERT INTO url_checks VALUES (url_id) = (%s);", [id])
+            # curs.execute("INSERT INTO url_checks (url_id) VALUES (1);")
             curs.execute("SELECT * FROM url_checks ORDER BY id DESC")
             url_id_row = curs.fetchmany(size=1)
+            conn.commit()
         conn.close()
 
         data_post = render_template('show.html', row=row, url_id_row=url_id_row)
